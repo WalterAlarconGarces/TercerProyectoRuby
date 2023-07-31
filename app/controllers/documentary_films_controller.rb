b8cf303c87e7d3694757dc3798ca962bdd59a4cd
Walter Alarcon Garces <walarcon@gmail.com>
2023-07-31 13:17:30 -0400

    creacion controlador documentary_film

--- app/controllers/documentary_films_controller.rb
+++ app/controllers/documentary_films_controller.rb
@@ -1,7 +1,26 @@
 class DocumentaryFilmsController < ApplicationController
   def index
+    @DocumentaryFilm = DocumentaryFilm.all
   end
-  def create
-    DocumentaryFilm.create(name: params[:name], synopsis:params[:synopsis], director:params[:director])
+  
+   def documentaryFilms_params
+    
+    params.require(:documentary_film).permit(:name, :synopsis, :director)
+   end
+   def create
+    
+    @DocumentaryFilm = DocumentaryFilm.new(documentaryFilms_params)
+    if @DocumentaryFilm.save
+   
+      redirect_to documentary_films_path
+    else
+   
+      render :new
+    end
+    
   end
+  def new
+    
+    @DocumentaryFilm = DocumentaryFilm.new
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   get 'movies/create', to: 'movies#create'
   get 'series', to: 'series#index'
   get 'series/create', to: 'series#create'
+
   get 'documentary_films', to: 'documentary_films#index'
   get 'documentary_films/create', to: 'documentary_films#create'
+  get 'documentary_films/new', to: 'documentary_films#new'
+  post 'documentary_films', to: 'documentary_films#create'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
