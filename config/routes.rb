fd60194d5c463ecf854e5df87041faebe6774986
Walter Alarcon Garces <walarcon@gmail.com>
2023-07-31 03:17:47 -0400

    creacion de vistas y rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'series/index'
-  get 'documentary_films/index'
+  get 'pages/index'
+  get 'movies', to: 'movies#index'
+  get 'series', to: 'series#index'
+  get 'documentary_films', to: 'documentary_films#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
 end
